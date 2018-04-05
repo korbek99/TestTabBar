cf97518bb9dc14cee12b701e8f7069ced1bea7d9
korbek100 <korbek100@gmail.com>
2018-04-05 20:11:38 -0300

    first commit

---
+++ SwiftTabBar/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  SwiftTabBar
+//
+//  Created by Jose David Bustos H on 05-04-18.
+//  Copyright © 2018 Jose David Bustos H. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
